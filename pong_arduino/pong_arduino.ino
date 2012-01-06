70aeaecfa01f45dd7c338ee79567bbabbe368b24
d.neumann.83@gmail.com <d.neumann.83@gmail.com@2661f1db-a1ee-58c5-5b3c-6242e6041d27>
2012-01-06 11:34:43 +0000

    [arduino] latest state (from the alpha test). dont know any changes

--- pong_arduino/pong_arduino.ino
+++ pong_arduino/pong_arduino.ino
@@ -3,8 +3,14 @@
 #define encoder0PinA  2
 #define encoder0PinB  4
 
+#define encoder0PinC  3
+#define encoder0PinD  5
+
 Bounce bouncerA = Bounce(encoder0PinA, 5);
 Bounce bouncerB = Bounce(encoder0PinB, 5);
+
+Bounce bouncerC = Bounce(encoder0PinC, 5);
+Bounce bouncerD = Bounce(encoder0PinD, 5);
 
 //pad identification bytes
 byte A01 = B000;
@@ -20,14 +26,24 @@ byte A08 = B111;
 
 int A01Value = 0;
 int A02Value;
-int A03Value;
+int A03Value = 0;
 int A04Value;
 int A05Value;
+int A06Value;
+int A07Value;
+int A08Value;
+
+boolean A_set;
+boolean B_set;
 
 //encoder variables
 int state, prevstate = 0, count = 0;
+int stateB, prevstateB = 0, countB = 0;
 int nextEncoderState[4] = { 2, 0, 3, 1 };
 int prevEncoderState[4] = { 1, 3, 0, 2 };
+
+static int MAX_VALUE = 70;
+static int MIN_VALUE = 0;
 
 // will store last time analog values has been sent
 long previousMillis = 0;
@@ -37,66 +53,110 @@ void setup() {
   digitalWrite(encoder0PinA, HIGH);       // turn on pullup resistor
   pinMode(encoder0PinB, INPUT); 
   digitalWrite(encoder0PinB, HIGH);       // turn on pullup resistor
+/*  
+  pinMode(encoder0PinC, INPUT); 
+  digitalWrite(encoder0PinC, HIGH);       // turn on pullup resistor
+  pinMode(encoder0PinD, INPUT); 
+  digitalWrite(encoder0PinD, HIGH);       // turn on pullup resistor
+*/
   
   attachInterrupt(0, readEncoder, CHANGE);
+ // attachInterrupt(1, readEncoderB, CHANGE);
   Serial.begin(57600);
 }
 
 //returs smoothed analog values
 void readAnalogValues(){
-    //A02Value = (0.9 * A02Value) + (0.1 * analogRead(2)) / 4;  //read analog pin 2 (2nd player)
-    //A03Value = (0.9 * A03Value) + (0.1 * analogRead(3)) / 4;  //read analog pin 3 (3rd player)
-    //A04Value = (0.9 * A04Value) + (0.1 * analogRead(4)) / 4;  //read analog pin 4 (4th player)
+    //A01Value = (0.9 * A01Value) + (0.1 * analogRead(1)) / 4;  //read analog pin 2 (1st player)
+    A02Value = (0.9 * A02Value) + (0.1 * analogRead(2)) / 4;  //read analog pin 3 (3rd player)
+    A04Value = (0.9 * A04Value) + (0.1 * analogRead(4)) / 4;  //read analog pin 4 (4th player)
     A05Value = (0.9 * A05Value) + (0.1 * analogRead(5)) / 4;  //read analog pin 5 (1st mic)
+    A06Value = (0.9 * A06Value) + (0.1 * analogRead(6)) / 4;  //read analog pin 5 (2nd mic)
+    A07Value = (0.9 * A07Value) + (0.1 * analogRead(7)) / 4;  //read analog pin 5 (3rd mic)
+    A08Value = (0.9 * A08Value) + (0.1 * analogRead(8)) / 4;  //read analog pin 5 (4th mic)
 }
 
 
 void loop() {   
   
     //delay with millis
-    unsigned long currentMillis = millis();
-    if(currentMillis - previousMillis > 500){
+/*    unsigned long currentMillis = millis();
+    if(currentMillis - previousMillis > 50){
       previousMillis = currentMillis;
 
       // read from the sensors
       readAnalogValues();
+          
+      //Serial.write(A01);
+      //Serial.write(A01Value);    
+                 
+      Serial.write(A02);
+      Serial.write(A02Value);
+      
+      Serial.write(A04);
+      Serial.write(A04Value);
       
       Serial.write(A05);
       Serial.write(A05Value);
-    } 
+      
+      Serial.write(A06);
+      Serial.write(A06Value);
+      
+      Serial.write(A07);
+      Serial.write(A07Value);
+      
+      Serial.write(A08);
+      Serial.write(A08Value);
+    } */
 }
 
 /* See this expanded function to get a better understanding of the
  * meanings of the four possible (pinA, pinB) value pairs:
  */
 void readEncoder(){
-  bouncerA.update();
-  bouncerB.update(); 
   
-  if (bouncerA.read() == HIGH) {   // found a low-to-high on channel A
-    if (bouncerB.read() == LOW) {  // check channel B to see which way
-                                             // encoder is turning
-      A01Value = A01Value - 1;         // CCW
-    } 
+    // Test transition
+  A_set = digitalRead(encoder0PinA) == HIGH;
+  // and adjust counter + if A leads B
+  A03Value += (A_set != B_set) ? +1 : -1;
+  
+  //check min & max thresholds
+  if(A03Value > 70) A03Value = MAX_VALUE;
+  else if(A03Value < 0) A03Value = MIN_VALUE;
+   
+  //Serial.write(A03);
+  Serial.println(A03Value);  
+}
+
+void readEncoderB(){
+  bouncerC.update();
+  bouncerD.update(); 
+  
+  if (bouncerC.read() == HIGH) {   // found a low-to-high on channel A
+    if (bouncerD.read() == LOW) {  // check channel B to see which way
+      //encoder turns CCW 
+      A01Value = A01Value - 1;
+    }
     else {
-      A01Value = A01Value + 1;         // CW
+      //encoder turns CW
+      A01Value = A01Value + 1;
     }
   }
-  else                                        // found a high-to-low on channel A
-  { 
-    if (bouncerB.read() == LOW) {   // check channel B to see which way
-                                              // encoder is turning  
-      A01Value = A01Value + 1;          // CW
+  else {                            // found a high-to-low on channel A
+    if (bouncerD.read() == LOW) {   // check channel B to see which way
+      //encoder turns CW 
+      A01Value = A01Value + 1;
     } 
     else {
-      A01Value = A01Value - 1;          // CCW
+      //encoder turns CCW 
+      A01Value = A01Value - 1;
     }
 
   }
   
   //check min & max thresholds
-  if(A01Value > 70) A01Value = 70;
-  else if(A01Value < 0) A01Value = 0;
+  if(A01Value > 70) A01Value = MAX_VALUE;
+  else if(A01Value < 0) A01Value = MIN_VALUE;
    
   Serial.write(A01);
   Serial.write(A01Value);  
